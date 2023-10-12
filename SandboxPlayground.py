20346676d0e5ce33eb207a51e138d2d888913949
MadelynSchool <02madelyn.hershberge@gulllakecs.org>
2023-10-12 08:28:17 -0400

    Add Print Lines, Introducing Myself

--- SandboxPlayground.py
+++ SandboxPlayground.py
@@ -1,3 +1,5 @@
 # Developer: Madelyn Hershberger
 # Date: 10.11.2023
 # Program: Sandbox Playground
+print("Hello Madelyn Hershberger")
+print ("\nI'm looking forward to the Taylor Swift movie on Friday")
